597f9ced4a42259e2a6aea906b5b9dcf229c75a4
arseniji <odiousguyfromnet@gmail.com>
2024-10-02 01:18:46 +0300

    5lab added

---
+++ KAiSD5lab/KAiSD5lab/Class1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class MyArrayList<T>
+{
+    private T[] _array;
+    private int length;
+    public MyArrayList()
+    {
+        this.length = 0;
+        _array = new T[length];
+    }
+
+    public MyArrayList(T[] array)
+    {
+        this.length = array.Length;
+        _array = new T[length];
+        for (int i = 0; i < array.Length; i++) _array[i] = array[i];
+    }
+    public MyArrayList(int capacity)
+    {
+        this.length = capacity;
+        _array = new T[length];
+    }
+
+    public void add(T e)
+    {
+        var new_array = new T[length + 1];
+        for (int i = 0; i < length; i++) new_array[i] = _array[i];
+        new_array[length] = e;
+        _array = new_array;
+        length = length + 1;
+    }
+    public void add(int index, T e)
+    {
+        var new_array = new T[length + 1];
+        for (int i = 0; i < index; i++) new_array[i] = _array[i];
+        new_array[index] = e;
+        for (int i = index + 1; i < length + 1; i++) new_array[i] = _array[i - 1];
+        _array = new_array;
+        length = length + 1;
+    }
+    public void addAll(T[] a)
+    {
+        var new_array = new T[length + a.Length];
+        for (int i = 0; i < length; i++) new_array[i] = _array[i];
+        for (int i = length; i < a.Length; i++) new_array[i] = a[i - length];
+        _array = new_array;
+        length = length + a.Length;
+    }
+    public void addAll(int index, T[] a)
+    {
+        var new_array = new T[length + a.Length];
+        for (int i = 0; i < index; i++) new_array[i] = _array[i];
+        for (int i = index; i < a.Length; i++) new_array[i] = a[i];
+        for (int i = index + 1; i < length + a.Length; i++) new_array[i] = a[i - length];
+        _array = new_array;
+        length = length + a.Length;
+    }
+    public void clear()
+    {
+        var new_array = new T[0];
+        length = 0;
+        _array = new_array;
+    }
+    public bool contains(object o)
+    {
+        foreach (var obj in _array)
+        {
+            if (obj.Equals(o)) { return true; }
+        }
+        return false;
+    }
+    public bool containsAll(T[] a)
+    {
+        foreach (var obj in a)
+        {
+            bool flag = false;
+            for (int i = 0; i < _array.Length; i++)
+            {
+                if (obj.Equals(_array[i])) { flag = true; break; }
+            }
+            if (flag == false) return false;
+        }
+        return true;
+    }
+    public bool isEmpty()
+    {
+        if (length == 0) return true;
+        else return false;
+    }
+    public void removeAll(T[] a)
+    {
+        foreach (var obj in a)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                if (obj.Equals(_array[i]))
+                {
+                    for (int j = i; j < length - 1; j++)
+                    {
+                        _array[j] = _array[j + 1];
+                    }
+                    length--;
+                }
+            }
+        }
+        var new_array = new T[length];
+        for (int i = 0; i < length; i++) { new_array[i] = _array[i]; }
+        _array = new_array;
+    }
+    public void retainAll(T[] a)
+    {
+        for (int i = 0; i < length; i++)
+        {
+            bool flag_1 = false;
+            foreach (var obj in a)
+            {
+                if (obj.Equals(_array[i])) { flag_1 = true; break; }
+            }
+            if (flag_1 == false)
+            {
+                for (int j = i; j < length - 1; j++)
+                {
+                    _array[j] = _array[j + 1];
+                }
+                length--;
+                i = 0;
+
+            }
+        }
+        var new_array = new T[length];
+        for (int i = 0; i < length; i++) { new_array[i] = _array[i]; }
+        _array = new_array;
+    }
+    public int size()
+    {
+        return length;
+    }
+    public T[] toArray()
+    {
+        var new_array = new T[length];
+        for (int i = 0; i < length; i++) new_array[i] = _array[i];
+        return new_array;
+    }
+    public T[] toArray(T[] a)
+    {
+        if (a == null) a = new T[length];
+        a = a.ToArray();
+        return a;
+    }
+    public T get(int index)
+    {
+        return _array[index];
+    }
+    public int indexOf(object o)
+    {
+        for (int i = 0; i < length; i++)
+        {
+            if (_array[i].Equals(o)) return i;
+        }
+        return -1;
+    }
+    public int lastIndexOf(object o)
+    {
+        int temp = -1;
+        for (int i = 0; i < length; i++)
+        {
+            if (_array[i].Equals(o)) temp = i;
+        }
+        return temp;
+    }
+    public T remove(int index)
+    {
+        for (int i = 0; i < length; i++)
+        {
+            if (_array[index].Equals(_array[i]))
+            {
+                for (int j = i; j < length - 1; j++)
+                {
+                    _array[j] = _array[j + 1];
+                }
+                length--;
+            }
+        }
+        var new_array = new T[length];
+        for (int i = 0; i < length; i++) { new_array[i] = _array[i]; }
+        _array = new_array;
+        return _array[index];
+    }
+    public void set(int index, T e)
+    {
+        _array[index] = e;
+    }
+    public T[] subList(int fromIndex, int toIndex)
+    {
+        int count = toIndex - fromIndex + 1;
+        var new_array = new T[count];
+        for (int i = fromIndex; i <= toIndex; i++) new_array[i - fromIndex] = _array[i];
+        return new_array;
+    }
+    public T this[int index]
+    {
+        get { return _array[index]; }
+        set { _array[index] = value; }
+    }
+
+}
---
+++ KAiSD5lab/KAiSD5lab/Program.cs
@@ -0,0 +1,85 @@
+﻿
+using System.Globalization;
+using System.IO;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+class lab5 
+{
+    private static string pathToData = @"..\..\..\input.txt";
+    static StreamReader sr = new StreamReader(pathToData);
+    static MyArrayList<string> readTags() 
+    {
+        bool openFlag = false;
+        bool closeFlag = false;
+        bool slashFlag = false;
+        var array = new MyArrayList<string>();
+        string tag = "";
+        string? line = sr.ReadLine();
+        while (line != null)
+        {
+            closeFlag = false;
+            tag = "";
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (line[i] == '<' && line[i + 1] != null)
+                {
+                    if (line[i + 1] == '/') { slashFlag = true; openFlag = true;}
+                    else if ((char.IsLetter(line[i + 1])) && !char.IsDigit(line[i + 1])) { slashFlag = false; openFlag = true; }
+                    else
+                    {
+                        do
+                        {
+                            if (i >= line.Length - 1) break;
+                            i++;
+                        } while (line[i + 1] != '<');
+                        tag = "";
+                    }
+                    if (i == line.Length) break;
+                }
+                if (slashFlag == false && line[i] == '/') { openFlag = false; tag = ""; }
+                if (slashFlag == true && line[i] == '/') { slashFlag = false; }
+                if (line[i] == '/' && char.IsDigit(line[i + 1])) openFlag = false;
+                if (line[i] == '>' && openFlag == true) { openFlag = false; tag += line[i]; closeFlag = true; }
+                if (openFlag && (line[i] == '<' || line[i] == '/'  || char.IsLetter(line[i]) || char.IsDigit(line[i]))) tag += line[i];
+                if (closeFlag == true) {array.add(tag); tag = ""; closeFlag = false;}
+            }
+            line = sr.ReadLine();
+        }
+        return array;
+    }
+    static MyArrayList<string> dellDub(MyArrayList<string> array)
+    {
+        var containsAll = new MyArrayList<string>();
+        var containsUnique = new MyArrayList<string>();
+        string line;
+        for (int i = 0; i < array.size(); i++)
+        {
+            line = array[i].ToLower().Replace("/",string.Empty);
+            containsAll.add(line);
+        }
+        for (int i = 0; i < array.size(); i++)
+        {
+            for (int j = i + 1; j < array.size(); j++)
+            {
+                if (containsAll[i] == containsAll[j]) array.set(j,"");    
+            }
+        }
+        for (int i = 0; i < array.size(); i++)
+        {
+            if (array[i] == "") continue;
+            containsUnique.add(array[i]);
+        }
+
+        return containsUnique;
+    }
+    public static void Main(string[] args)
+    {
+        var mas = new MyArrayList<string>();
+        var sas = new MyArrayList<string>();
+        mas = readTags();
+        sas = dellDub(mas);
+        for (int i = 0; i < sas.size(); i++) Console.WriteLine($"{i + 1}) {sas[i]}");
+    }
+}
