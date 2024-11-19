ff285f0ef542eea728f34303be39c3f0584fbb98
arseniji <odiousguyfromnet@gmail.com>
2024-11-19 03:14:36 +0300

    clearer syntaxis, silly mistakes deleted

--- KAiSD4ex/KAiSD4ex/Program.cs
+++ KAiSD4ex/KAiSD4ex/Program.cs
@@ -1,219 +1,234 @@
-﻿class KAiSD
+﻿namespace KAiSD
 {
     class MyArrayList<T>
     {
-        private T[] _array;
-        private int length;
+        private T[] ElementData;
+        int ElementCount { get; set; }
         public MyArrayList()
         {
-            this.length = 0;
-            _array = new T[length];
+            ElementCount = 0;
+            ElementData = new T[0];
         }
-
         public MyArrayList(T[] array)
         {
-            this.length = array.Length;
-            _array = new T[length];
-            for (int i = 0; i < array.Length; i++) _array[i] = array[i];
+            ElementCount = array.Length;
+            ElementData = new T[array.Length];
+            for (int i = 0; i < array.Length; i++) ElementData[i] = array[i];
         }
         public MyArrayList(int capacity)
         {
-            this.length = capacity;
-            _array = new T[length];
+            ElementCount = 0;
+            ElementData = new T[capacity];
+        }
+        public void Add(T e)
+        {
+            if (ElementCount >= ElementData.Length)
+            {
+                var new_array = new T[(int)(ElementCount * 1.5) + 1];
+                for (int i = 0; i < ElementCount; i++) new_array[i] = ElementData[i];
+                new_array[ElementCount] = e;
+                ElementData = new_array;
+                ElementCount = ElementCount + 1;
+            }
+            else
+            {
+                ElementData[ElementCount++] = e;
+            }
         }
 
-        public void add(T e)
+        public void Add(int index, T e)
         {
-            var new_array = new T[length + 1];
-            for (int i = 0; i < length; i++) new_array[i] = _array[i];
-            new_array[length] = e;
-            _array = new_array;
-            length = length + 1;
-        }
-        public void add(int index, T e)
-        {
-            var new_array = new T[length + 1];
-            for (int i = 0; i < index; i++) new_array[i] = _array[i];
+            var new_array = new T[ElementCount + 1];
+            for (int i = 0; i < index; i++) new_array[i] = ElementData[i];
             new_array[index] = e;
-            for (int i = index + 1; i < length + 1; i++) new_array[i] = _array[i - 1];
-            _array = new_array;
-            length = length + 1;
+            for (int i = index + 1; i < ElementCount + 1; i++) new_array[i] = ElementData[i - 1];
+            ElementData = new_array;
+            ElementCount = ElementCount + 1;
         }
-        public void addAll(T[] a)
+        public void AddAll(T[] a)
         {
-            var new_array = new T[length + a.Length];
-            for (int i = 0; i < length; i++) new_array[i] = _array[i];
-            for (int i = length; i < a.Length; i++) new_array[i] = a[i - length];
-            _array = new_array;
-            length = length + a.Length;
+            var new_array = new T[ElementCount + a.Length];
+            for (int i = 0; i < ElementCount; i++) new_array[i] = ElementData[i];
+            for (int i = ElementCount; i < a.Length; i++) new_array[i] = a[i - ElementCount];
+            ElementData = new_array;
+            ElementCount = ElementCount + a.Length;
         }
-        public void addAll(int index, T[] a)
+        public void AddAll(int index, T[] a)
         {
-            var new_array = new T[length + a.Length];
-            for (int i = 0; i < index; i++) new_array[i] = _array[i];
+            var new_array = new T[ElementCount + a.Length];
+            for (int i = 0; i < index; i++) new_array[i] = ElementData[i];
             for (int i = index; i < a.Length; i++) new_array[i] = a[i];
-            for (int i = index + 1; i < length + a.Length; i++) new_array[i] = a[i - length];
-            _array = new_array;
-            length = length + a.Length;
+            for (int i = index + 1; i < ElementCount + a.Length; i++) new_array[i] = a[i - ElementCount];
+            ElementData = new_array;
+            ElementCount = ElementCount + a.Length;
         }
-        public void clear()
+        public void Clear()
         {
             var new_array = new T[0];
-            length = 0;
-            _array = new_array;
+            ElementCount = 0;
+            ElementData = new_array;
         }
-        public bool contains(object o)
+        public bool Contains(T o)
         {
-            foreach (var obj in _array)
+            foreach (var obj in ElementData)
             {
                 if (obj.Equals(o)) { return true; }
             }
             return false;
         }
-        public bool containsAll(T[] a)
+        public bool ContainsAll(T[] a)
         {
             foreach (var obj in a)
             {
                 bool flag = false;
-                for (int i = 0; i < _array.Length; i++)
+                for (int i = 0; i < ElementCount; i++)
                 {
-                    if (obj.Equals(_array[i])) { flag = true; break; }
+                    if (obj.Equals(ElementData[i])) { flag = true; break; }
                 }
                 if (flag == false) return false;
             }
             return true;
         }
-        public bool isEmpty()
+        public bool IsEmpty()
         {
-            if (length == 0) return true;
+            if (ElementCount == 0) return true;
             else return false;
         }
-        public void removeAll(T[] a)
+        public void RemoveAll(T[] a)
         {
             foreach (var obj in a)
             {
-                for (int i = 0; i < length; i++)
+                for (int i = 0; i < ElementCount; i++)
                 {
-                    if (obj.Equals(_array[i]))
+                    if (obj.Equals(ElementData[i]))
                     {
-                        for (int j = i; j < length - 1; j++)
+                        for (int j = i; j < ElementCount - 1; j++)
                         {
-                            _array[j] = _array[j + 1];
+                            ElementData[j] = ElementData[j + 1];
                         }
-                        length--;
+                        ElementCount--;
                     }
                 }
             }
-            var new_array = new T[length];
-            for (int i = 0; i < length; i++) { new_array[i] = _array[i]; }
-            _array = new_array;
+            var new_array = new T[ElementCount];
+            for (int i = 0; i < ElementCount; i++) { new_array[i] = ElementData[i]; }
+            ElementData = new_array;
         }
-        public void retainAll(T[] a)
+        public void RetainAll(T[] a)
         {
-            for (int i = 0; i < length; i++)
+            for (int i = 0; i < ElementCount; i++)
             {
                 bool flag_1 = false;
                 foreach (var obj in a)
                 {
-                    if (obj.Equals(_array[i])) { flag_1 = true; break; }
+                    if (obj.Equals(ElementData[i])) { flag_1 = true; break; }
                 }
                 if (flag_1 == false)
                 {
-                    for (int j = i; j < length - 1; j++)
+                    for (int j = i; j < ElementCount - 1; j++)
                     {
-                        _array[j] = _array[j + 1];
+                        ElementData[j] = ElementData[j + 1];
                     }
-                    length--;
+                    ElementCount--;
                     i = 0;
 
                 }
             }
-            var new_array = new T[length];
-            for (int i = 0; i < length; i++) { new_array[i] = _array[i]; }
-            _array = new_array;
+            var new_array = new T[ElementCount];
+            for (int i = 0; i < ElementCount; i++) { new_array[i] = ElementData[i]; }
+            ElementData = new_array;
         }
-        public int size()
+        public int Size()
         {
-            return length;
+            return ElementCount;
         }
-        public T[] toArray()
+        public T[] ToArray()
         {
-            var new_array = new T[length];
-            for (int i = 0; i < length; i++) new_array[i] = _array[i];
+            var new_array = new T[ElementCount];
+            for (int i = 0; i < ElementCount; i++) new_array[i] = ElementData[i];
             return new_array;
         }
-        public T[] toArray(T[] a)
+        public T[] ToArray(T[] a)
         {
-            if (a == null) a = new T[length];
+            if (a == null) a = new T[ElementCount];
             a = a.ToArray();
             return a;
         }
-        public T get(int index)
+        public T Get(int index)
         {
-            return _array[index];
+            return ElementData[index];
         }
-        public int indexOf(object o)
+        public int IndexOf(object o)
         {
-            for (int i = 0; i < length; i++)
+            for (int i = 0; i < ElementCount; i++)
             {
-                if (_array[i].Equals(o)) return i;
+                if (ElementData[i].Equals(o)) return i;
             }
             return -1;
         }
-        public int lastIndexOf(object o)
+        public int LastIndexOf(object o)
         {
             int temp = -1;
-            for (int i = 0; i < length; i++)
+            for (int i = 0; i < ElementCount; i++)
             {
-                if (_array[i].Equals(o)) temp = i;
+                if (ElementData[i].Equals(o)) temp = i;
             }
             return temp;
         }
-        public T remove(int index)
+        public T Remove(int index)
         {
-            for (int i = 0; i < length; i++)
+            for (int i = 0; i < ElementCount; i++)
             {
-                if (_array[index].Equals(_array[i]))
+                if (ElementData[index].Equals(ElementData[i]))
                 {
-                    for (int j = i; j < length - 1; j++)
+                    for (int j = i; j < ElementCount - 1; j++)
                     {
-                        _array[j] = _array[j + 1];
+                        ElementData[j] = ElementData[j + 1];
                     }
-                    length--;
+                    ElementCount--;
                 }
             }
-            var new_array = new T[length];
-            for (int i = 0; i < length; i++) { new_array[i] = _array[i]; }
-            _array = new_array;
-            return _array[index];
+            var new_array = new T[ElementCount];
+            for (int i = 0; i < ElementCount; i++) { new_array[i] = ElementData[i]; }
+            ElementData = new_array;
+            return ElementData[index];
         }
-        public void set(int index, T e) 
+        public void Remove(object o)
         {
-            _array[index] = e;
+            for (int i = 0; i < ElementCount; i++)
+            {
+                if (o.Equals(ElementData[i]))
+                {
+                    for (int j = i; j < ElementCount - 1; j++)
+                    {
+                        ElementData[j] = ElementData[j + 1];
+                    }
+                    ElementCount--;
+                }
+            }
+            var new_array = new T[ElementCount];
+            for (int i = 0; i < ElementCount; i++) { new_array[i] = ElementData[i]; }
+            ElementData = new_array;
         }
-        public T[] subList(int fromIndex, int toIndex) 
+        public void Set(int index, T e)
+        {
+            ElementData[index] = e;
+        }
+        public T[] SubList(int fromIndex, int toIndex)
         {
             int count = toIndex - fromIndex + 1;
             var new_array = new T[count];
-            for (int i = fromIndex; i <= toIndex; i++) new_array[i-fromIndex] = _array[i];
+            for (int i = fromIndex; i <= toIndex; i++) new_array[i - fromIndex] = ElementData[i];
             return new_array;
         }
         public T this[int index]
         {
-            get { return _array[index]; }
-            set { _array[index] = value; }
+            get { return ElementData[index]; }
+            set { ElementData[index] = value; }
         }
-
-    }
-    public static void Main(string[] args)
-    {
-        int[] a = { 1, 2, 3, 4, 3 };
-        int[] c = { };
-        var mas = new MyArrayList<int>(c);
-        mas.addAll(a);
-        for (int i = 0; i < mas.size(); i++)
+        public int Length
         {
-            Console.WriteLine(mas[i]);
+            get { return ElementData.Length; }
         }
     }
 }
